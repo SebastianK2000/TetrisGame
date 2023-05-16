5990b4a8d5d06d8863a4522791c7b7b487e0d8f8
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2023-05-16 22:34:39 +0200

    Add PNG and modified TetrisBasic.js, not ready to test

--- TetrisBasic.js
+++ TetrisBasic.js
@@ -4,6 +4,10 @@ let gBArrayHeight = 20;
 let gBArrayWidth = 12;
 let startX = 4;
 let startY = 0;
+let score = 0;
+let level = 1;
+let winOrLose = "Playing";
+let tetrisLogo;
 let coordinateArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth)
 .fill(0));
 let curTetromino = [[1,0], [0,1], [1,1], [2,1]];
@@ -14,6 +18,9 @@ let tetrominosColors = ['purple', 'cyan', 'blue', 'yellow', 'orange',
 let curTetrominoColor;
 
 let GameBoardArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth)
+.fill(0));
+
+let stoppedShapeArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth)
 .fill(0));
 
 let DIRECTION = {
@@ -61,12 +68,43 @@ function SetupCanvas(){
     ctx.strokeStyle = 'black';
     ctx.strokeRect(8, 8, 280, 462);
 
+    tetrisLogo = new Image(161, 54);
+    tetrisLogo.onload = DrawTetrisLogo;
+    tetrisLogo.src = "tetrislogo.png";
+
+    ctx.fillStyle = 'black';  
+    ctx.font = '21px Arial';
+    ctx.fillText("SCORE", 300, 98);
+
+    ctx.strokeRect(300, 107, 161, 24);
+
+    ctx.fillText(score.toString(), 310, 127);
+
+    ctx.fillText("LEVEL", 300, 157);
+    ctx.strokeRect(300, 171, 161, 24);
+    ctx.fillText(level.toString(), 310, 190);
+
+    ctx.fillText("WIN / LOSE", 300, 221);
+    ctx.fillText(winOrLose, 310, 261);
+    ctx.strokeRect(300, 232, 161, 95);
+    ctx.fillText("CONTROLS", 300, 354);
+    ctx.strokeRect(300, 366, 161, 104);
+    ctx.font = '19px Arial';
+    ctx.fillText("A : Move Left", 310, 388);
+    ctx.fillText("D : Move Right", 310, 413);
+    ctx.fillText("S : Move Down", 310, 438);
+    ctx.fillText("E : Rotate Right", 310, 463);
+
     document.addEventListener('keydown', HandleKeyPress);
     CreateTetrominos();
     CreateTetromino();
 
     CreateCoordArray();
     DrawTetromino();
+}
+
+function DrawTetrisLogo(){
+    ctx.drawImage(tetrisLogo, 300, 8, 161, 54);
 }
 
 function DrawTetromino(){
@@ -81,22 +119,38 @@ function DrawTetromino(){
 }
 
 function HandleKeyPress(key){
-    if(key.keyCode === 65) {
-        direction = DIRECTION.LEFT;
-        DeleteTetromino();
-        startX--;
-        DrawTetromino();
+    if(winOrLose != "Game Over"){
+        if(key.keyCode === 65){
+            direction = DIRECTION.LEFT;
+        if(!HittingTheWall() && !CheckForHorizontalCollision()){
+            DeleteTetromino();
+            startX--;
+            DrawTetromino();
+        }
+
     } else if(key.keyCode === 68){
         direction = DIRECTION.RIGHT;
-        DeleteTetromino();
-        startX++;
-        DrawTetromino();
+        if(!HittingTheWall() && !CheckForHorizontalCollision()){
+            DeleteTetromino();
+            startX++;
+            DrawTetromino();
+        }
+
     } else if(key.keyCode === 83){
-        direction = DIRECTION.DOWN;
-        DeleteTetromino();
-        startY++;
-        DrawTetromino();
+        MoveTetrominoDown();
+    } else if(key.keyCode === 69){
+        RotateTetromino();
     }
+}
+}
+
+function MoveTetrominoDown(){
+        direction = DIRECTION.DOWN;
+        if(!CheckForVerticalCollison()){
+            DeleteTetromino();
+            startY++;
+            DrawTetromino();
+        }
 }
 
 function DeleteTetromino(){
@@ -133,4 +187,153 @@ function CreateTetromino(){
     let randomTetromino = Math.floor(Math.random() * tetrominos.length);
     curTetromino = tetrominos[randomTetromino];
     curTetrominoColor = tetrominosColors[randomTetromino];
+}
+
+function HittingTheWall(){
+    for(let i = 0; i < curTetromino.length; i++){
+        let newX = curTetromino[i][0] + startX;
+        if(newX <= 0 && direction === DIRECTION.LEFT){
+            return true;
+        } else if(newX >= 11 && direction === DIRECTION.RIGHT){
+            return true;
+        }
+    }
+    return false;
+}
+
+function CheckForVerticalCollison(){
+    let tetrominoCopy = curTetromino;
+    let collision = false;
+    for(let i = 0; i < tetrominoCopy.length; i++) {
+        let square = tetrominoCopy[i];
+        let x = square[0] + startX;
+        let y = square[1] + startY;
+        if(direction === DIRECTION.DOWN){
+            y++;
+        }
+
+        if(gameBoardArray[x][y+1] === 1){
+                if(typeof stoppedShapeArray[x][y+1] === 'string'){
+                DeleteTetromino();
+                startY++;
+                DrawTetromino();
+                collision = true;
+                break;
+            }
+            if(y >= 20){
+                collision = true;
+                break;
+            }
+        }
+
+        if(collision){
+            if(startY <= 2){
+                winOrLose = "Game Over";
+                ctx.fillStyle = 'white';
+                ctx.fillRect(310, 242, 140, 30);
+                ctx.fillStyle = 'black';
+                ctx.fillText(winOrLose, 310, 261);
+            } else{
+                for(let i = 0; i < tetrominoCopy.length; i++){
+                    let square = tetrominoCopy[i];
+                    let x = square[0] + startX;
+                    let y = square[1] + startY;
+                    stoppedShapeArray[x][y] = curTetrominoColor;
+                }
+                CheckForCompleteRows();
+                CreateTetromino();
+                direction = DIRECTION.IDLE;
+                startX = 4;
+                startY = 0;
+                DrawTetromino();
+            }
+        }
+    }
+
+}
+
+function CheckForHorizontalCollision(){
+    let tetrominoCopy = curTetromino;
+    let collision = false;
+    for(let i = 0; i < tetrominoCopy.length; i++){
+        let square = tetrominoCopy[i];
+        let x = square[0] + startX;
+        let y = square[1] + startY;
+
+        if(direction === DIRECTION.LEFT){
+            x--;
+        } else if(direction === DIRECTION.RIGHT){
+            x++;
+        }
+
+        var stoppedShapeVal = stoppedShapeArray[x][y];
+        if(typeof stoppedShapeVal === 'string'){
+            collision = true;
+            break;
+        }
+    }
+    return collision;
+}
+
+function CheckForCompleteRows(){
+    let rowsToDelete = 0;
+    let startOfDeletion = 0; 
+    for(let y = 0; y < gBArrayHeight; y++){
+        let completed = true;
+        for(let x = 0; x < gBArrayWidth; x++){
+            let square = stoppedShapeArray[x][y];
+            if(square === 0 || (typeof square === 'undefined')){
+                completed = false;
+                break;
+            }
+        }
+        
+        if(completed){
+            if(startOfDeletion === 0) startOfDeletion = y;
+            rowsToDelete++;
+            for(let i = 0; i < gBArrayWidth; i++){
+                stoppedShapeArray[i][y] = 0;
+                gameBoardArray[i][y] = 0;
+                let coorX = coordinateArray[i][y].x;
+                let coorY = coordinateArray[i][y].y;
+                ctx.fillStyle = 'white';
+                ctx.fillRect(coorX, coorY, 21, 21);
+            }
+        }
+    }
+
+    if(rowsToDelete > 0){
+        score += 10;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(310, 242, 140, 30);
+        ctx.fillStyle = 'black';
+        ctx.fillText(score.toString(), 310, 261);
+        MoveAllRowsDown(rowsToDelete, startOfDeletion);
+    }
+}
+
+function MoveAllRowsDown(rowsToDelete, startOfDeletion){
+    for(var i = startOfDeletion - 1; i >= 0; i--){
+        for(var x = 0; x < gBArrayWidth; x++){
+            var y2 = i + rowsToDelete;
+            var square = stoppedShapeArray[x][i];
+            var nextSquare = stoppedShapeArray[x][y2];
+            if(typeof square === 'string'){
+                nextSquare = square;
+                gameBoardArray[x][y2] = 1;
+                stoppedShapeArray[x][y2] = square;
+                let coorX = coordinateArray[x][y2].x;
+                let coorY = coordinateArray[x][y2].y;
+                ctx.fillStyle = nextSquare;
+                ctx.fillRect(coorX, coorY, 21, 21);
+                square = 0;
+                gameBoardArray[x][i] = 0;
+                stoppedShapeArray[x][i] = 0;
+                coorX = coordinateArray[x][i].x;
+                coorY = coordinateArray[x][i].y;
+                ctx.fillStyle = 'white';
+                ctx.fillRect(coorX, coorY, 21, 21);
+            }
+        }
+    }
 }
